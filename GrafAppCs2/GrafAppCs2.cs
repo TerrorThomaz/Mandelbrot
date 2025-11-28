60b238ec6fe086c6f31c7e82c9255cb3e790e3c9
TerrorThomaz <thomaskoemans1@gmail.com>
2025-11-28 12:32:26 +0100

    geeft error als ik pull dus push eerst

--- GrafAppCs2/GrafAppCs2.cs
+++ GrafAppCs2/GrafAppCs2.cs
@@ -17,47 +17,85 @@ afbeelding.Location = new Point(10, 10);
 afbeelding.Size = new Size(200, 200);
 afbeelding.BackColor = Color.White;
 afbeelding.Image = plaatje;
-void mandel(object o ,EventArgs ea)
+
+double calc_x_min = -2.0;
+double calc_y_min = -2.0;
+double calc_x_max = 2.0;
+double calc_y_max = 2.0;
+
+void zoom (object o, MouseEventArgs ea) {
+
+    
+    double calc_x_stap = (calc_x_max - calc_x_min) / plaatje.Width;
+    double calc_y_stap = (calc_y_max - calc_y_min) / plaatje.Height;
+
+    double clicked_x = calc_x_min + ea.X * calc_x_stap;
+    double clicked_y = calc_y_min + ea.Y * calc_y_stap;
+
+    double factor = 0.5;
+
+    double width = (calc_x_max - calc_x_min) * factor;
+    double height = (calc_y_max - calc_y_min) * factor;
+    
+    calc_x_min = clicked_x - width / 2;
+    calc_x_max = clicked_x + width / 2;
+    calc_y_min = clicked_y - height / 2;
+    calc_y_max = clicked_y + height / 2;
+
+    mandel(null, EventArgs.Empty);
+    
+    
+}
+
+
+int mandel_berekening(double calc_x_schaal ,double calc_y_schaal, int x, int y)
 {
+    double calc_x = calc_x_min + calc_x_schaal * x;
+
+    double calc_y = calc_y_min + calc_y_schaal * y;
+    double pyth = 0;
+    double a = 0;
+    double b = 0;
+    int i = 0;
+    while (pyth < 2 && i < 4000)
+    {
+        double n_a = a * a - b * b + calc_x;
+        double n_b = 2 * a * b + calc_y;
+        b = n_b;
+        a = n_a;
+        pyth = Math.Sqrt((a * a) + (b * b));
+
+        i = i + 1;
+    }
+    return i;
+}
+
+
+
+void mandel(object o, EventArgs ea)
+{
+    double calc_x_stap = (calc_x_max - calc_x_min) / plaatje.Width;
+    double calc_y_stap = (calc_y_max - calc_y_min) / plaatje.Height;
     for (int y = 0; y < plaatje.Height; y++)
     {
         for (int x = 0; x < plaatje.Width; x++)
         {
-
-            double calc_x_stap = 4d / plaatje.Width;
-            double calc_x = calc_x_stap * x - 2;
-            double calc_y_stap = 4d / plaatje.Height;
-            double calc_y = calc_y_stap * y - 2;
-            double pyth = 0;
-            double a = 0;
-            double b = 0;
-            int i = 0;
-            while (pyth < 2 && i < 400)
-            {
-                double n_a = a * a - b * b + calc_x;
-                double n_b = 2 * a * b + calc_y;
-                b = n_b;
-                a = n_a;
-                pyth = Math.Sqrt((a * a) + (b * b));
-
-                i = i + 1;
-            }
+            int i = mandel_berekening(calc_x_stap, calc_y_stap, x, y);
+            
             if (i % 2 == 0)
             {
                 plaatje.SetPixel(x, y, Color.Black);
-                afbeelding.Invalidate();
             }
             else
             {
                 plaatje.SetPixel(x, y, Color.White);
-                afbeelding.Invalidate();
             }
         }
     }
     afbeelding.Refresh();
 }
 
-
+afbeelding.MouseClick += zoom;
 
 Button knop = new Button();
 scherm.Controls.Add(knop);
